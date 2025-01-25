ed8d78b93e8209694b3cfd22d6d62da01cb02701
Souhaiel <karbaa.souheil@gmail.com>
2025-01-25 15:10:33 +0900

    made cv downloadable instead of view mode

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -16,7 +16,7 @@ export default function HomePage() {
   }, []);
 
   const resumeUrl =
-    "https://drive.google.com/file/d/1yQHjAh-MC7-ifhMGM__oX0rJ6WSaFbln/view?usp=sharing";
+    "https://drive.google.com/uc?export=download&id=1yQHjAh-MC7-ifhMGM__oX0rJ6WSaFbln";
 
 
   return (
